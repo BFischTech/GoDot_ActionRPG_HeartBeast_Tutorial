226044079238bcea1afcf35bc22363a907b47191
BFischTech <bfischtech@gmail.com>
2022-03-15 14:55:00 -0400

    basic enemy, animation and hurtbox plus knockback

--- GoDot ActionRPG Tutorial - HeartBeast/Player/Player.gd
+++ GoDot ActionRPG Tutorial - HeartBeast/Player/Player.gd
@@ -18,9 +18,11 @@ var roll_vector = Vector2.DOWN
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 #delta - time last frame took to process (physics property, basically 1/60)  delta can change because of framerate
 
@@ -38,12 +40,14 @@ func _physics_process(delta):
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
+	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
