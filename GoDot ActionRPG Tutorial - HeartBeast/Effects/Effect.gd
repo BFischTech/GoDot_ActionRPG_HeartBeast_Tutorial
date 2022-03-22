4c8b8ae48db9403df6b55444efa42e31a60fd583
BFischTech <bfischtech@gmail.com>
2022-03-22 11:30:39 -0400

    refactored some code, enemy now explodes - knockback is broken and can't discern why.  continuing with tutorial and will monitor

---
+++ GoDot ActionRPG Tutorial - HeartBeast/Effects/Effect.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite
+
+onready var animatedSprite = $AnimatedSprite
+
+func _ready():
+	self.connect("animation_finished", self, "_on_animation_finished")
+	# frame = 0 - technically not required, but if in would force game to always play sprite starting at frame 0 
+	play("Animate")
+	
+
+func _on_animation_finished():
+	queue_free()
--- GoDot ActionRPG Tutorial - HeartBeast/Player/Player.gd
+++ GoDot ActionRPG Tutorial - HeartBeast/Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION = 550
-const MAX_SPEED =  100
-const FRICTION = 550
-const ROLL_SPEED = 125
+export var ACCELERATION = 550
+export var MAX_SPEED =  100
+export var FRICTION = 550
+export var ROLL_SPEED = 125
 
 enum {
 	MOVE,
@@ -66,12 +66,12 @@ func move_state(delta):
 	if Input.is_action_just_pressed("roll"):
 		state = ROLL
 		
-func roll_state(delta):
+func roll_state(_delta):
 	velocity = roll_vector * ROLL_SPEED
 	animationState.travel("Roll")
 	move()
 	
-func attack_state(delta):
+func attack_state(_delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
