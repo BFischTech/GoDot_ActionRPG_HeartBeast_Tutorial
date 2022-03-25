aacbdd2229b76ff5672747333ed52f1b6789e408
BFischTech <bfischtech@gmail.com>
2022-03-25 13:11:14 -0400

    bats can hit player now, player has health

--- GoDot ActionRPG Tutorial - HeartBeast/Enemies/Bat.gd
+++ GoDot ActionRPG Tutorial - HeartBeast/Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = CHASE
 onready var sprite = $Sprite
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var stats = $Stats
+onready var hurtbox = $Hurtbox
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -37,7 +38,6 @@ func _physics_process(delta):
 			var player = playerDetectionZone.player
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
-				
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 			else: state = IDLE
 			sprite.flip_h = velocity.x < 0
@@ -51,7 +51,7 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = (area.knockback_vector * 120)
-	
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
--- GoDot ActionRPG Tutorial - HeartBeast/Stats.gd
+++ GoDot ActionRPG Tutorial - HeartBeast/Stats.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-export(int) var max_health = 3
+export(int) var max_health = 1
 onready var health = max_health setget set_health
 
 signal no_health
